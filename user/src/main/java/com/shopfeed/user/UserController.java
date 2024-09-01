67f49655c1c7b50d6ef38c876c7cc238fc15d1aa
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-02 07:59:24 +0800

    update annotations

--- user/src/main/java/com/shopfeed/user/UserController.java
+++ user/src/main/java/com/shopfeed/user/UserController.java
@@ -1,21 +1,21 @@
 package com.shopfeed.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(path = "/users")
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping(path = "/add")
-    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+    public String addNewUser(@RequestParam String name, @RequestParam String email) {
         User n = new User();
         n.setName(name);
         n.setEmail(email);
@@ -24,7 +24,7 @@ public class UserController {
     }
 
     @GetMapping(path = "/all")
-    public @ResponseBody Iterable<User> getAllUsers() {
+    public Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 }
