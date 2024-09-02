e5b69a912885cc45d3297d5b6b924e5fa8a73280
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-02 09:07:52 +0800

    finish user crud

--- user/src/main/java/com/shopfeed/user/UserController.java
+++ user/src/main/java/com/shopfeed/user/UserController.java
@@ -1,11 +1,18 @@
 package com.shopfeed.user;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/users")
@@ -14,17 +21,36 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping(path = "/add")
-    public String addNewUser(@RequestParam String name, @RequestParam String email) {
-        User n = new User();
-        n.setName(name);
-        n.setEmail(email);
-        userRepository.save(n);
-        return "Saved";
-    }
-
-    @GetMapping(path = "/all")
-    public Iterable<User> getAllUsers() {
+    @GetMapping
+    public Iterable<User> getAll() {
         return userRepository.findAll();
     }
+
+    @GetMapping(path = "/{id}")
+    public User get(@PathVariable(value = "id") UUID id) {
+        return userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find user with id: " + id.toString()));
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping(path = "/{id}")
+    public User update(@PathVariable UUID id, @RequestBody User updatedUser) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find user with id: " + id.toString()));
+        user.setName(updatedUser.getName());
+        user.setEmail(updatedUser.getEmail());
+
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String Delete(@PathVariable UUID id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find user with id: " + id.toString()));
+        userRepository.delete(user);
+
+        return "User has been deleted";
+    }
+
 }
