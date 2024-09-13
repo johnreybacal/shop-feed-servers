427e5a27a1650bc4657b49824198876c2bf33bec
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-13 20:33:22 +0800

    setup messenger
    enqueue does not support symfony 7 yet

--- shop/src/Entity/Shop.php
+++ shop/src/Entity/Shop.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\ShopRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Component\Uid\Uuid;
