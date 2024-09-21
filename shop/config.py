104f9bd1c4e265dd30fbcc46933f3bbd570626e3
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-21 13:14:13 +0800

    setup env file

---
+++ shop/config.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    db_url: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
